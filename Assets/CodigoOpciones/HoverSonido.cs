a2ce759725df68865c9193b7e0375f5ab29111db
Liquix7 <liquix97099@gmail.com>
2025-05-22 16:26:51 +0200

    MenuCoompleto

---
+++ Assets/CodigoOpciones/HoverSonido.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverSonido : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource audioSource;
+    public AudioClip hoverClip;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (audioSource != null && hoverClip != null) { 
+            audioSource.PlayOneShot(hoverClip);
+        } 
+    }
+}
